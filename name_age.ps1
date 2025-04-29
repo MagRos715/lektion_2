c4e0631cfbaf6a31b04d18994bfc600df61d249a
Magnus Rosenlund <magnus.rosenlund.student@ksgyf.se>
2025-04-29 09:04:05 +0200

    1.1

---
+++ name_age.ps1
@@ -0,0 +1,13 @@
+# Create 2 variables and add them to print result
+
+# variable for name
+$name = "Alice"
+
+# variable for age
+$age = 25
+
+# concat
+
+
+# Print variables
+Write-host "Hi $name, are you $age years old?"
