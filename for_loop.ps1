f6bca925a5de2b12105362cf066c84ed03d8334d
Magnus Rosenlund <magnus.rosenlund.student@ksgyf.se>
2025-04-29 10:51:27 +0200

    while

---
+++ for_loop.ps1
@@ -0,0 +1,5 @@
+# count 1 to 5
+
+for ($i = 1; $i -le 100; $i++) {
+    Write-Host "Nummer: $i"
+}
---
+++ while_loop.ps1
@@ -0,0 +1,8 @@
+# count 5 t 1
+
+$i = 5
+
+while ($i -gt 0) {
+    Write-Host "Nedräkning: $i"
+    $i--
+}
